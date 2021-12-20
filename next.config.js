b1cdecdeaada8db66c1564c1ff09d63c66ff6028
alimoneib <ali_moneib@yahoo.com>
2021-12-20 09:56:08 +0200

    removed w=majority from mongodb_uri

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ const withCSS = require('@zeit/next-css');
 module.exports = {
   webpack5: false,
   env: {
-    MONGODB_URI: `'mongodb+srv://ecomDev:ZEknPrfm0nudmFOu@cluster0.druxa.mongodb.net/ecom-dev?retryWrites=true&w=majority'`,
+    MONGODB_URI: `'mongodb+srv://ecomDev:ZEknPrfm0nudmFOu@cluster0.druxa.mongodb.net/ecom-dev?retryWrites=true'`,
     JWT_SECRET:
       'eyJhbGciOiJIUzI1NiJ9.eyJSb2xlIjoiQWRtaW4iLCJJc3N1ZXIiOiJJc3N1ZXIiLCJVc2VybmFtZSI6IkphdmFJblVzZSIsImV4cCI6MTYzNjAzNTYwMywiaWF0IjoxNjM2MDM1NjAzfQ.pjdqjIb2Weut6eecX_78jnrF_9EIT-eWiAGJsUynL78',
   },
