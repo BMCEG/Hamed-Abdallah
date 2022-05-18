588cde89119c9154baa37da4b7a463b7b7e7e592
mohammed-haroun <mohammed1.haroun1997@gmail.com>
2022-05-18 20:45:43 +0200

    add facebook pixel

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import { SnackbarProvider } from 'notistack';
 import React from 'react';
 import PropTypes from 'prop-types';
 import Head from 'next/head';
+import Script from 'next/script';
 import { ThemeProvider } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import theme from '../src/theme';
@@ -23,6 +24,23 @@ export default function MyApp(props) {
   return (
     <React.Fragment>
       <Head>
+      <Script
+  strategy="afterInteractive"
+  dangerouslySetInnerHTML={{
+    __html: `
+    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer', 'GTM-XXXXXX');
+  `,
+  }}
+/>
+      <noscript>
+ <img height="1" width="1" 
+src="https://www.facebook.com/tr?id=689405399107163&ev=PageView
+&noscript=1"/>
+</noscript>
         <title>Hamed Abdallah Optics</title>
         <meta
           name="viewport"
